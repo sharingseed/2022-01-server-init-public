c77a18e7780f7ac01374f1d442b5df2c69fcb9dd
Noname <noname@localhost>
2023-12-29 10:13:16 +0000

    update

--- scripts/docker/install-docker-compose.sh
+++ scripts/docker/install-docker-compose.sh
@@ -1,6 +1,8 @@
 set -eu
 echo "-- $(basename $(dirname $0))/$(basename $0) --" >&2
 
+# 2023/12 deprecated, should use "docker compose"
+
 # Install docker-compose
 if [ -x "$(command -v docker-compose)" ]; then
   echo "skip install docker-compose" >&2
@@ -9,7 +11,7 @@ else
     sudo apt-get update
     sudo apt-get install -y python3-pip
   fi
-  sudo -H pip3 install docker-compose
+  pip3 install docker-compose
 fi
 
 echo "-- completed --" >&2
