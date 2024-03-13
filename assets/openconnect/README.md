8546c642a4758577fe64bc12fda6c8e09c33e296
SS_Dev_Team <toyosawa@sharingseed.co.jp>
2024-03-13 22:35:29 +0000

    update

---
+++ assets/openconnect/README.md
@@ -0,0 +1,17 @@
+
+```sh
+# start
+docker compose up -d
+
+# test proxy
+curl --proxy 'socks5h://127.0.0.1:8022' 'https://api.ipify.org/'
+
+# reload danted
+docker compose exec openconnect service danted restart
+
+# add user
+docker compose exec openconnect bash -c "
+adduser --shell /sbin/nologin --gecos '' preview
+echo 'preview:cftyuhbvg' | chpasswd
+"
+```
--- assets/openconnect/init.sh
+++ assets/openconnect/init.sh
@@ -2,10 +2,11 @@
 set -ex
 
 apt update -y
-apt-get install -y openconnect openssh-client openssh-server netcat
+apt-get install -y openconnect dante-server netcat
 rm -rf /var/cache/apt/* /tmp/* /var/tmp/*
 
 # Get server cert
+echo '>> Prepare for starting VPN...' >&2
 set +ex
 SERVER_RES=$(echo $VPN_PASSWORD | openconnect -q --cookieonly --passwd-on-stdin -u ${VPN_USER} --servercert "" ${VPN_OPTIONS} ${VPN_URL} 2>&1)
 set -ex
@@ -32,32 +33,16 @@ fi
 
 echo '>> Starting VPN...' >&2
 echo $COOKIE | openconnect -b --cookie-on-stdin --servercert ${SERVER_CERT} ${VPN_OPTIONS} ${VPN_URL}
-echo '>> VPN connected.' >&2
 
-# Make ssh tunnel
-echo '>> Starting SSH Server...' >&2
-if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-  ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ""
-fi
-if [ ! -d /run/sshd ]; then
-  mkdir -p /run/sshd
-fi
-/usr/sbin/sshd -D &
+echo '>> Sleep 5s...' >&2
+sleep 5
+ip add show tun0
 
-echo ">> Waiting for sshd to start..."
-until nc -z -w3 localhost 22; do echo "sshd is unavailable - waiting" && sleep 2 ; done
-echo ">> sshd is ready"
+echo '>> Starting Socks Server...' >&2
+cat /etc/_danted.conf > /etc/danted.conf
+service danted restart
 
-echo ">> Starting tunnel to ${TUNNEL_TARGET}..."
-if [ ! -f /root/.ssh/id_rsa ]; then
-  ssh-keygen -t rsa -f /root/.ssh/id_rsa -N ""
-  cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-fi
-# TUNNEL_OPTIONS="-gfCN4 -L0.0.0.0:10001:163.220.177.15:22 -D 10002"
-ssh \
-  -o ServerAliveInterval=60 \
-  -o StrictHostKeyChecking=no \
-  -o UserKnownHostsFile=/dev/null \
-  -i /root/.ssh/id_rsa root@localhost \
-  ${TUNNEL_OPTIONS}
-echo ">> tunnel is ready"
+adduser --shell /sbin/nologin --gecos '' preview
+echo 'preview:cftyuhbvg' | chpasswd
+
+echo '>> Ready.' >&2
