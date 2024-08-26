f66e3306d354be901272407d2bbead97b074d10d
SS_Dev_Team <toyosawa@sharingseed.co.jp>
2024-08-26 19:52:27 +0000

    update

--- scripts/python/install-uv.sh
+++ scripts/python/install-uv.sh
@@ -1,15 +1,15 @@
 set -eu
 echo "-- $(basename $(dirname $0))/$(basename $0) --" >&2
 
-if [ -n "$(command -v rye)" ]; then
-  echo "skip install rye" >&2
+if [ -n "$(command -v uv)" ]; then
+  echo "skip install uv" >&2
 else
-  curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION="--yes" bash
-  source "$HOME/.rye/env"
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  source "$HOME/.cargo/env"
 fi
 
 # FILE="$HOME/.profile"
-# SCRIPT='source "$HOME/.rye/env"'
+# SCRIPT='export PATH=$PATH:$HOME/.cargo/bin'
 # if [ -n "$(grep "${SCRIPT}" "${FILE}")" ]; then
 #   echo "skip append ${FILE}" >&2
 # else
